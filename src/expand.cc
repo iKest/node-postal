edcbd8225f2ba79e8d390fa29bc42e2bccbf5bb0
iKest <ikestster@gmail.com>
2019-03-07 23:52:57 +0300

    Update expand.cc

--- src/expand.cc
+++ src/expand.cc
@@ -118,8 +118,7 @@ NAN_METHOD(ExpandAddress) {
     v8::Local<v8::Array> ret = Nan::New<v8::Array>(num_expansions);
 
     for (i = 0; i < num_expansions; i++) {
-        v8::Local<v8::String> e = Nan::New<String>(expansions[i]).ToLocalChecked();
-        ret->Set(i, e);
+        ret->Set(i, Nan::New<String>(expansions[i]).ToLocalChecked());
         free(expansions[i]);
     }
     
